b8752b142ab2503723cbe4953f3af8aac152d48f
ASUS <ASUS@study>
2024-05-31 10:11:11 -0500

    Proyecto

---
+++ src/main/java/proyecto/proyectofinalpoo/ProyectoFinalPOO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package proyecto.proyectofinalpoo;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ProyectoFinalPOO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
